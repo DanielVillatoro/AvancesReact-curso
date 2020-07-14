9c30235996c3d678ab922538ad258b8df4222ca3
DanielVillatoro <danielvillatoroc@gmail.com>
2020-07-14 00:36:24 -0600

    Update index.js

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,14 +1,2 @@
-//Operador condicional ternario
 
-const activo=true;
-// let mensaje='';
-// if(!activo){
-//     mensaje='activo';
-// }
-// else{
-//     mensaje='inactivo'
-// }
-
-// const mensaje=(activo)?'Activo':'Inactivo'
-const mensaje=!activo && 'Activo'
-console.log(mensaje);
+const saludo='Hola';
