180f2cdb91a853129ad86df6db9d510828b49187
DanielVillatoro <danielvillatoroc@gmail.com>
2020-07-20 22:00:11 -0600

    Publicación de App en GitHubPages

---
+++ 04-gif-expert-app/docs/precache-manifest.c5dfd2f9a151e085b74dba73e410c5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1981cd5835b4c872c37124fd95e37df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "514c71cce7a4e17de43e",
+    "url": "/static/css/main.8f06f644.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514c71cce7a4e17de43e",
+    "url": "/static/js/main.ccba9dd3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
